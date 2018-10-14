409918d0d36128dcba5704548c09270b4fed6213
Derek Rodger <drodger@gmail.com>
2018-10-14 13:32:56 -0700

    1.1 Add ability to specify output filename

--- lessons/load_and_display.py
+++ lessons/load_and_display.py
@@ -3,10 +3,12 @@ import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
+ap.add_argument("-o", "--output", required=True, help="Filename to save the image as")
 args = vars(ap.parse_args())
 
 # load the iamge and display some basic information on it
 image = cv2.imread(args["image"])
+print(f"shape: {image.shape}")
 print(f"width: {image.shape[1]}")
 print(f"height: {image.shape[0]}")
 print(f"channels: {image.shape[2]}")
@@ -15,4 +17,4 @@ cv2.imshow("Image", image)
 cv2.waitKey(0)
 
 # save the image -- OpenCV handles converting the filetypes
-cv2.imwrite("newimage.jpg", image)
+cv2.imwrite(args["output"], image)
