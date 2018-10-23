9702aa8f6286a113f22e02e2677f3f4b15b1b7b9
Derek Rodger <drodger@gmail.com>
2018-10-23 06:13:24 -0700

    1.10.2 Edge detection

---
+++ lessons/auto_canny.py
@@ -0,0 +1,22 @@
+import argparse
+import imutils
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale, and blur it slightly
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+
+wide = cv2.Canny(blurred, 10, 200)
+tight = cv2.Canny(blurred, 225, 250)
+auto = imutils.auto_canny(blurred)
+
+cv2.imshow("Original", image)
+cv2.imshow("Wide Edge Map", wide)
+cv2.imshow("Tight Edge Map", tight)
+cv2.imshow("Auto Edge Map", auto)
+cv2.waitKey(0)
