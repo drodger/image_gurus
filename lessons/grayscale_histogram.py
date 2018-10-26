1873beb7bd2b901b3adb5afbbc04d4a38bc8ee35
Derek Rodger <drodger@gmail.com>
2018-10-26 15:54:30 -0700

    1.12 Histograms

---
+++ lessons/grayscale_histogram.py
@@ -0,0 +1,37 @@
+from matplotlib import pyplot as plt
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Original", image)
+cv2.imshow("Grayscale", gray)
+
+# construct a grayscale histogram
+hist = cv2.calcHist([image], [0], None, [256], [0, 256])
+
+# plot the histogram
+plt.figure()
+plt.title("Grayscale Histogram")
+plt.xlabel("Bins")
+plt.ylabel("# of Pixels")
+plt.plot(hist)
+plt.xlim([0, 256])
+
+# normalize the histogram
+hist /= hist.sum()
+
+# plot the normalized histogram
+plt.figure()
+plt.title("Grayscale Histogram (Normalized)")
+plt.xlabel("Bins")
+plt.ylabel("% of Pixels")
+plt.plot(hist)
+plt.xlim([0, 256])
+plt.show()
